75d40333e426974ad8a1e98e9793984594451959
lolItsHaiech <seyedamin13845@gmail.com>
2025-03-24 22:43:06 +0330

    style: reformat some files
    added and removed some empty lines.
    + added blank=False to email field in model.

--- user/models.py
+++ user/models.py
@@ -5,4 +5,5 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField("email", blank=True, unique=True)
-    phone_number = models.CharField("phone_number", max_length=11, unique=True, validators=[RegexValidator("^09\d{9}$")])
+    phone_number = models.CharField("phone_number", max_length=11, blank=False, unique=True,
+                                    validators=[RegexValidator(r"^09\d{9}$")])
