ea4b96de7e3c8acde5a4bb0fd4c21d6e32c84de7
fazel <mohamadfazel1382@gmail.com>
2025-03-25 17:37:15 +0330

    feat(user): add custom user model for authentication

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "phone_number")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
+
+    list_display = ("username", "email", "phone_number", "first_name", "last_name", "is_staff")
+
+
+admin.site.register(User, CustomUserAdmin)
