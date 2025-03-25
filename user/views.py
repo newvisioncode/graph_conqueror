f4985fa253e9382bf93506142812c9e9d4ea7fbe
fazel <mohamadfazel1382@gmail.com>
2025-03-25 17:45:07 +0330

    feat(user): add signup endpoint

--- user/views.py
+++ user/views.py
@@ -1,1 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import SignupSerializer
+
+User = get_user_model()
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignupSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'user': serializer.data,
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+        }, status=status.HTTP_201_CREATED, headers=headers)
