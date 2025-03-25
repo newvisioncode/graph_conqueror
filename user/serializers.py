245479ca3b5cde7e033c3a03f5032ae3534e1b65
fazel <mohamadfazel1382@gmail.com>
2025-03-25 17:45:07 +0330

    feat(user): add signup endpoint

---
+++ user/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(label="confirm_password", write_only=True, max_length=128)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'phone_number', 'password', 'confirm_password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match")
+        validate_password(data['password'])
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        return User.objects.create_user(**validated_data)
