34a943db4464f7a560b402d7df4d6dc55a0eb0fd
fazel <mohamadfazel1382@gmail.com>
2025-03-26 18:12:40 +0330

    feat(castle_graph): add castle submission and submissionItem and captureCastle model

--- castle_graph/admin.py
+++ castle_graph/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from castle_graph.models import Submission, Castle, CaptureCastle, SubmissionItem
+
 # Register your models here.
+admin.site.register(Submission)
+admin.site.register(Castle)
+admin.site.register(SubmissionItem)
+admin.site.register(CaptureCastle)
--- castle_graph/models.py
+++ castle_graph/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.utils.translation import gettext_lazy as _
+from question.models import Question, QuestionItem
 
 User = get_user_model()
 
@@ -11,3 +14,37 @@ class ContestGroup(models.Model):
 class ContestUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.ForeignKey(ContestGroup, on_delete=models.CASCADE)
+
+
+class Castle(models.Model):
+    castle_name = models.CharField(max_length=100, unique=True, blank=False, null=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    score = models.IntegerField(blank=False, null=False)
+    neighbors = ArrayField(models.CharField(max_length=100, blank=False), blank=False, null=False)
+
+
+class Submission(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    group = models.ForeignKey(ContestGroup, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    castle = models.ForeignKey(Castle, on_delete=models.CASCADE)
+
+
+class SubmissionItem(models.Model):
+    submission = models.ForeignKey(Submission, on_delete=models.CASCADE, blank=False, null=False)
+    question_item = models.ForeignKey(QuestionItem, on_delete=models.CASCADE, blank=False, null=False)
+    token = models.CharField(max_length=255, blank=False, null=False)
+    result = models.CharField(max_length=255, blank=True, null=True)
+    runtime = models.IntegerField(blank=True, null=True)
+
+
+class CaptureCastle(models.Model):
+    class CaptureCause(models.IntegerChoices):
+        SOLVED = 0, _("Solved")
+        CONQUERED = 1, _("Conquered")
+
+    castle = models.ForeignKey(Castle, on_delete=models.CASCADE, blank=False, null=False)
+    group = models.ForeignKey(ContestGroup, on_delete=models.CASCADE, blank=False, null=False)
+    submission = models.OneToOneField(Submission, on_delete=models.CASCADE, blank=True, null=True)
+    cause = models.IntegerField(blank=False, null=False, choices=CaptureCause.choices, default=CaptureCause.SOLVED)
+    created = models.DateTimeField(auto_now_add=True)
